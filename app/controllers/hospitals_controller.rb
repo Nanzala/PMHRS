b829133c22545fc8b939b701ce75d03543c30e69
Nanzala <tabithamayabi@gmail.com>
2015-11-17 14:59:36 +0000

    Changes in hospital entity

---
+++ app/controllers/hospitals_controller.rb
@@ -0,0 +1,22 @@
+class HospitalsController < ApplicationController
+  
+  def new
+	@hospital = Hospital.new
+  end
+
+  def create
+	@hospital = Hospital.new(hospital_params)
+	@hospital.save
+	render text: "Successfully saved!!!"
+
+  end
+
+  def hospital_params
+  	params.require(:hospital).permit(:id,
+  									 :name,
+  									 :location,
+  									 :level,
+  									 :address)
+  end
+
+end
