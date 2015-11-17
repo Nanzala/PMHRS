9513468a3166d1500f7dbcf6e133ea597e0b36a0
Nanzala <tabithamayabi@gmail.com>
2015-11-17 14:59:36 +0000

    Changes in hospital entity

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'hospitals/new'
+
+	resources :hospitals
+	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
