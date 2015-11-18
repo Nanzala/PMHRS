f37b35989860559a03af9c16c45c263e5d9a18e1
Kate Kimani <katewkimani@gmail.com>
2015-11-18 22:08:56 +0000

    Adds the staff controller and view files.

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,7 +1,11 @@
 class Staff < ActiveRecord::Base
 
-    validates :name, presence:true
+  validates  :name, presence: true
+  validates  :phone, presence: true, 
+             length: { within: 10..12 }
+  validates  :address, presence: true
+  validates  :role, presence: true
 
-    belongs_to :records
+  belongs_to :records
     
 end
