3d8a50d1f4fbe160aa9efbef5a95ec9237136b35
Kate Kimani <katewkimani@gmail.com>
2015-11-22 00:41:32 +0000

    Adds the patient controller and view files.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,30 @@
+class PatientsController < ApplicationController
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    
+    if @patient.save
+      flash[:notice] = "Patient registered successfully"
+      redirect_to @patient
+    end
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+   params.require(:patient).permit(
+    :name,
+    :gender,
+    :date_of_birth,
+    :phone,
+    :email,
+    :next_of_kin,
+    :next_of_kin_contact 
+   )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'patients/new'
+
   get 'staffs/show'
 
   get 'staffs/new'
 
     resources :staffs
-
+    resources :patients
 end
