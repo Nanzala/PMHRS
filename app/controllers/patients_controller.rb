eee77f9a706483000e464843059ba9402606283c
Kate Kimani <katewkimani@gmail.com>
2015-11-22 21:09:40 +0000

    Adds the update staff & patient features.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -19,6 +19,23 @@ class PatientsController < ApplicationController
     @patient = Patient.find(params[:id])
   end
 
+  def edit
+    @patient = Patient.find(params[:id])
+    render 'new'
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+
+    if @patient.update(patient_params)
+     flash[:notice] = "Patient updated successfully"
+     redirect_to @patient
+   else
+     flash.now[:alert] = "Patient not updated"
+     render 'new'
+   end  
+  end
+
   def patient_params
    params.require(:patient).permit(
     :name,
