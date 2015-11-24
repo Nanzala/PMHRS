3998d0deb562a99dbcc8ba8f561f319760f136f7
Sam Nyavor <samnyavor@gmail.com>
2015-11-24 10:33:12 +0000

    Added SSN column to Staff(Reset table and run migration)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123210350) do
+ActiveRecord::Schema.define(version: 20151124102719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,9 @@ ActiveRecord::Schema.define(version: 20151123210350) do
     t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "ssn",        null: false
   end
+
+  add_index "staffs", ["ssn"], name: "index_staffs_on_ssn", using: :btree
 
 end
