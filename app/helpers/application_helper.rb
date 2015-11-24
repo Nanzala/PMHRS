a0fb7962a4b02028e3cca184fdef9d4016777c15
Sam Nyavor <samnyavor@gmail.com>
2015-11-24 18:45:51 +0000

    Add Patient done properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-	def full_title(page_title = '')
+    def full_title(page_title = '')
         base_title = 'PMHRS'
         if page_title.empty?
             base_title
         else
             page_title = base_title =   base_title + ' | ' + page_title
         end
-    	end
+    end
+
 end
