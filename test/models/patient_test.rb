7abc34477966ac1072af1e02aef9d584714ebe3a
Kate Kimani <katewkimani@gmail.com>
2015-11-26 20:46:12 +0000

    Updates the test fixtures files.

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -4,4 +4,10 @@ class PatientTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "correct patient" do
+    @patient = Patient.new(name: 'test patient', gender: 'female', date_of_birth: '12-12-2012', phone: '0201234567', email: 'patient@mail.com', next_of_kin: 'parent', next_of_kin_contact: '0201234132', ssn: '123123123')
+    @patient.save
+    assert @patient.valid?
+  end
 end
