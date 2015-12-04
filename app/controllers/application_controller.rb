b1ff1236e201de55e3cc2c49c06cda05572083ba
Sam Nyavor <samnyavor@gmail.com>
2015-12-04 15:37:50 +0000

    Started Working on admin dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  def raise_404(string="Not found")
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    def raise_404(string="Not found")
         raise ActionController::RoutingError.new(string)
-  end
+    end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
-  def home
-  end
+    before_action :authenticate_admin!
+    def home
+    end
 end
