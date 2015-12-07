6b5e5acb3e3474c5a1cb2379e698b9bea623b075
Kate Kimani <katewkimani@gmail.com>
2015-12-07 19:32:36 +0000

    Adds the validation and tests for the record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,8 +1,12 @@
 class Record < ActiveRecord::Base
   validates :patient_id,  presence:true
   validates :staff_id,    presence:true
+  validates :height,      presence:true, numericality: { greater_than: 0, less_than: 240 }
+  validates :weight,      presence:true, numericality: { greater_than: 0 }
+  validates :temperature, presence:true, numericality: true
 
   belongs_to :patient
+  validates_associated :patient
   has_many :hospitals, through: :permissions
   has_many :staffs
   has_many :medications
--- test/models/record_test.rb
+++ test/models/record_test.rb
@@ -1,12 +1,8 @@
 require 'test_helper'
 
 class RecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   def setup
-    #@record = Record.new(staff_id: '1', height: nil, weight: nil, temperature: nil, blood_pressure: nil, symptoms: nil, diagnosis: nil, patient_id: 1)
     @record = Record.new( staff_id: "657b2a62-2973-41fa-9a24-945612643358", height: 165, weight: 54, temperature: 38, blood_pressure: "117/90", symptoms: "Fever, Rashes", diagnosis: "Heat Exposure", patient_id: "7d31fcfd-775c-4420-a274-3183ec4911a5")
   end
 
@@ -21,6 +17,41 @@ class RecordTest < ActiveSupport::TestCase
 
   test "patient id not blank" do
     @record.patient_id = nil
+    assert_not @record.valid?
+  end
+
+  test "patient height not blank" do
+    @record.height = nil
+    assert_not @record.valid?
+  end
+
+  test "patient weight not blank" do
+    @record.weight = nil
+    assert_not @record.valid?
+  end
+
+  test "patient temperature not blank" do
+    @record.temperature = nil
+    assert_not @record.valid?
+  end
+
+  test "patient height not zero" do
+    @record.height = 0
+    assert_not @record.valid?
+  end
+
+  test "patient height is less than 240cm" do
+    @record.height = 240
+    assert_not @record.valid?
+  end
+
+  test "patient weight not zero" do
+    @record.weight = 0
+    assert_not @record.valid?
+  end
+
+  test "patient id exists in the system" do
+    @record.patient_id = 3
     assert_not @record.valid?
   end
 end
