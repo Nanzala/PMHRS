4a8c211e841fa8e6fcee0f889f633132838facc4
Nanzala <tabithamayabi@gmail.com>
2015-12-11 08:47:15 +0000

    UI updates.

--- config/routes.rb
+++ config/routes.rb
@@ -2,33 +2,34 @@ Rails.application.routes.draw do
 
     root 'app#home'
 
-    devise_for :admin, 
-        :controllers => { 
+    devise_for :admin,
+        :controllers => {
         registrations: 'admins/registrations',
-        sessions: 'admins/sessions'
+        sessions: 'admins/sessions',
+        passwords: 'admins/passwords'
     },
-    path: "system", path_names: { 
-        sign_in: 'login', 
-        sign_out: 'logout', 
-        confirmation: 'verify', 
-        unlock: 'unblock', 
-        registration: 'register', 
+    path: "system", path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        confirmation: 'verify',
+        unlock: 'unblock',
+        registration: 'register',
         sign_up: 'signup'
-    } 
+    }
 
-    devise_for :staffs, 
-        :controllers => { 
+    devise_for :staffs,
+        :controllers => {
         registrations: 'staffs/registrations',
         sessions: 'staffs/sessions'
     },
-    path: "app", path_names: { 
-        sign_in: 'login', 
-        sign_out: 'logout', 
-        confirmation: 'verify', 
-        unlock: 'unblock', 
-        registration: 'register', 
+    path: "app", path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        confirmation: 'verify',
+        unlock: 'unblock',
+        registration: 'register',
         sign_up: 'signup'
-    } 
+    }
 
     get 'reset' => 'app#reset'
     get 'system' => 'dashboard#home'
@@ -36,7 +37,7 @@ Rails.application.routes.draw do
 
     namespace :staffs do
         resources :staffs
-        resources :staff_signup 
+        resources :staff_signup
         get 'applications' => 'staff_signup#staff_applications'
     end
 
